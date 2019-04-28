6f08acc5eb2a1a6988d33bf7a3e52578c3299b4e
Desmond Rivet <desmond.rivet@gmail.com>
2019-04-28 11:59:40 -0400

    Add test code for media endpoint

--- indieweb_server/config_dev.py
+++ indieweb_server/config_dev.py
@@ -1,4 +1,4 @@
 WEBSITE_URL = 'https://desmondrivet.com'
-UPLOAD_FOLDER = '/path/wwwroot/media'
+UPLOAD_FOLDER = 'upload_folder'
 WEBSITE_CONTENTS = 'https://api.github.com/repos/drivet/website-test/contents'
 WEBMENTION_FOLDER = '/content/webmentions'
