549b422deb9c4732fa87cde0ab47b28abdbd4838
Desmond Rivet <desmond.rivet@gmail.com>
2019-05-01 07:59:27 -0400

    change media endpoint to be subroute of micropub

--- indieweb_server/__init__.py
+++ indieweb_server/__init__.py
@@ -6,6 +6,6 @@ from indieweb_server.webmention import webmention_bp
 
 app = Flask(__name__)
 app.config.from_envvar('INDIEWEB_SETTINGS')
-app.register_blueprint(media_bp, url_prefix='/media')
+app.register_blueprint(media_bp, url_prefix='/micropub/media')
 app.register_blueprint(micropub_bp, url_prefix='/micropub')
 app.register_blueprint(webmention_bp, url_prefix='/webmention')
